f0a0e86b9f1654bb5f6311cdf929c3d59b55664c
FloridaTechYoung <132877561+FloridaTechYoung@users.noreply.github.com>
2025-12-10 16:25:33 -0500

    change name

--- sql/stock_check.sql
+++ sql/stock_check.sql
@@ -1,6 +1,7 @@
 
 -- check if unitsInStock is bigger than reorderLevel if not, add to table + don't add when product is discontinued
-select productID
+CREATE TABLE Reorder_list AS
+SELECT productID
 FROM companyDataTable
 WHERE 
     unitsInStock < reorderLevel
