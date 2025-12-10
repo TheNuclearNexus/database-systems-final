2138aea2b0613d6ff581c53acfb744f6fc7959f9
FloridaTechYoung <132877561+FloridaTechYoung@users.noreply.github.com>
2025-12-10 16:30:29 -0500

    updated table usage

--- sql/stock_check.sql
+++ sql/stock_check.sql
@@ -2,7 +2,7 @@
 -- check if unitsInStock is bigger than reorderLevel if not, add to table + don't add when product is discontinued
 CREATE TABLE Reorder_list AS
 SELECT productID
-FROM companyDataTable
+FROM Products
 WHERE 
     unitsInStock < reorderLevel
     AND 
